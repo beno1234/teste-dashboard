fa2699b6eee147cef071496bba7ba1fb4be09674
beno1234 <benolopesdias@gmail.com>
2023-03-30 18:50:10 -0300

    backend

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,28 +3,23 @@ import nextConnect from "next-connect";
 const apiRoute = nextConnect({
   // Handle any other HTTP method
   onNoMatch(req, res) {
-    const resObj = { error: `Method '${req.method}' Not Allowed` };
+    const resObj = { error: `Method ${req.method} Not Allowed` }
     res.statusCode = 405;
-    res.end(JSON.stringify(resObj));
+    res.write(JSON.stringify(resObj));
+    res.end();
   },
   onError(error, _, res) {
-    const resObj = { error: `Sorry something Happened! ${error.message}` };
+    const resObj = { error: `Sorry something Happened! ${error.message}` }
     res.statusCode = 501;
-    res.end(JSON.stringify(resObj));
-  },
+    res.write(JSON.stringify(resObj));
+    res.end();
+  }
 });
 
 apiRoute.get(async (req, res) => {
-  try {
-    const response = await fetch("http://benotestehello.us-3.evennode.com/");
-    const data = await response.json();
-    res.statusCode = response.status;
-    res.end(JSON.stringify(data));
-  } catch (error) {
-    console.error(error);
-    res.statusCode = 500;
-    res.end({ msg: "Error processing request" });
-  }
+  res.statusCode = 200
+  res.write("Hello World")
+  res.end()
 });
 
 export default apiRoute;
