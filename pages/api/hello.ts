56e9fe35ee32acdc79e3b60abf25f564e72c328e
beno1234 <benolopesdias@gmail.com>
2023-03-30 19:04:45 -0300

    backend

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -21,7 +21,7 @@ apiRoute.get(async (req, res) => {
     res.statusCode = 200
     res.write("Hello World")
     res.end()
-  }, 10000); // espera 5 segundos antes de enviar a resposta
+  }, 5000); // espera 5 segundos antes de enviar a resposta
 });
 
 export default apiRoute;
