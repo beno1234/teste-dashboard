8008c56c4d348c7aaf12f95b322df891429836b0
beno1234 <benolopesdias@gmail.com>
2023-03-30 19:14:55 -0300

    backend

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,27 +1,5 @@
-import nextConnect from "next-connect";
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const apiRoute = nextConnect({
-  // Handle any other HTTP method
-  onNoMatch(req, res) {
-    const resObj = { error: `Method ${req.method} Not Allowed` }
-    res.statusCode = 405;
-    res.write(JSON.stringify(resObj));
-    res.end();
-  },
-  onError(error, _, res) {
-    const resObj = { error: `Sorry something Happened! ${error.message}` }
-    res.statusCode = 501;
-    res.write(JSON.stringify(resObj));
-    res.end();
-  }
-});
-
-apiRoute.get(async (req, res) => {
-  setTimeout(() => {
-    res.statusCode = 200
-    res.write("Hello World")
-    res.end()
-  }, 10000); // espera 5 segundos antes de enviar a resposta
-});
-
-export default apiRoute;
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: 'Hello World' });
+}
